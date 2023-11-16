41628ca5753e802138a5af71aa2d48795d9c3701
cptntommy <141337672+cptntommy@users.noreply.github.com>
2023-11-16 12:11:33 +0000

    Create csv_filter.py
    Created main python file to allow a user to input a csv file, and then filter by a fixed value. User can then export filtered dataset as a csv file.

---
+++ csv_filter.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Data Filtering App')
+
+# File upload
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    # Read the CSV data into a DataFrame
+    df = pd.read_csv(uploaded_file)
+
+    # User inputs for filtering
+    column_to_filter = st.selectbox('Select the column to filter on:', df.columns)
+    unique_values = df[column_to_filter].unique()
+    value_to_filter = st.selectbox(f'Select the value to filter in {column_to_filter}:', unique_values)
+
+    # Filter the DataFrame
+    filtered_df = df[df[column_to_filter] == value_to_filter]
+
+    # Display the results
+    st.write('Filtered Data:')
+    st.dataframe(filtered_df)
+
+    # Convert DataFrame to CSV for download
+    def convert_df_to_csv(df):
+        return df.to_csv(index=False).encode('utf-8')
+
+    csv = convert_df_to_csv(filtered_df)
+
+    st.download_button(
+        label="Download filtered data as CSV",
+        data=csv,
+        file_name='filtered_data.csv',
+        mime='text/csv',
+    )
+else:
+    st.write("Upload a CSV file to get started.")
